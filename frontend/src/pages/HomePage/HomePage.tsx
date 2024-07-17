d36bf8f093c83615653c67ca673fc0689502617d
LestterX <lgs.alves@hotmail.com>
2024-07-17 00:36:02 -0300

    feat: add router provider

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,23 +1,26 @@
 import Footer from "../../components/Footer/Footer";
 import ProductsTable from "../../components/ProductsTable/ProductsTable";
 import TopBar from "../../components/TopBar/TopBar";
+import { SearchProvider } from "../../context/SearchProvider";
 
 function HomePage() {
     return (
-        <main className="w-full flex-col">
-            <nav className="w-full h-20 bg-slate-500 mb-1">
-                <TopBar />
-            </nav>
-            <section className="bg-slate-500 max-w-5xl my-0 mx-auto text-white p-3 text-center font-bold text-4xl">
-                <h1>PRODUTOS</h1>
-            </section>
-            <section className="w-full max-w-5xl h-screen my-2 mx-auto bg-slate-300 border-solid border-slate-600 rounded-lg">
-                <ProductsTable />
-            </section>
-            <section className="w-full h-20 bg-slate-500 my-0 mx-auto">
-                <Footer />
-            </section>
-        </main>
+        <SearchProvider>
+            <main className="w-full flex-col">
+                <nav className="w-full h-20 bg-slate-500 mb-1">
+                    <TopBar />
+                </nav>
+                <section className="bg-slate-500 max-w-5xl my-0 mx-auto text-white p-3 text-center font-bold text-4xl">
+                    <h1>PRODUTOS</h1>
+                </section>
+                <section className="w-full max-w-5xl h-screen my-2 mx-auto bg-slate-300 border-solid border-slate-600 rounded-lg">
+                    <ProductsTable />
+                </section>
+                <section className="w-full h-20 bg-slate-500 my-0 mx-auto">
+                    <Footer />
+                </section>
+            </main>
+        </SearchProvider>
     );
 }
 
